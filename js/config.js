ce8f4c906973bdb27a8361369b5eb49d570e9ba9
entercompu <entercompu.ar@gmail.com>
2026-07-13 14:40:06 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Pegá acá la URL que te da Google Apps Script al implementar la Aplicación web.
 // Ejemplo: "https://script.google.com/macros/s/AKfycb.../exec"
-const SCRIPT_URL = "https://script.google.com/macros/s/TU_ID_AQUI/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxSvwqNyam26tXxbA4pkQFblEhy-bPlVC59GiGB3POqxOTJpRLrHnb2ItarfSqKonb5ZA/exec";
