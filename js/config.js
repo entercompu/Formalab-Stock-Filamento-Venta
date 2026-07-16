42a348fbd3d9e0268862f82556b72d6c8faa6ed4
entercompu <entercompu.ar@gmail.com>
2026-07-16 14:36:06 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 // Pegá acá la URL que te da Google Apps Script al implementar la Aplicación web.
 // Ejemplo: "https://script.google.com/macros/s/AKfycb.../exec"
-const SCRIPT_URL = "PEGA_ACA_TU_URL_DE_APPS_SCRIPT";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxSvwqNyam26tXxbA4pkQFblEhy-bPlVC59GiGB3POqxOTJpRLrHnb2ItarfSqKonb5ZA/exec";
 
 // Número de WhatsApp del negocio para recibir pedidos del catálogo público.
 // Formato: código de país + número, SIN espacios, SIN el signo +.
 // Ejemplo Argentina (Río Gallegos): "5492966123456"
-const WHATSAPP_NUMBER = "PEGA_TU_NUMERO_DE_WHATSAPP";
+const WHATSAPP_NUMBER = "+5492966474022";
